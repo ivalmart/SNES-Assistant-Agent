0b854a77a37eded1485fec17928a923b26ed55c7
Glitch (snes-agent-ui) <none>
2024-10-16 20:33:20 +0000

    🧀🍓 Checkpoint ./README.md:70889106/86 ./index.html:70889106/4308 ./snes.mjs:70889106/9200

---
+++ README.md
@@ -0,0 +1,1 @@
+Like https://emulator-agent-export.glitch.me/ but without being tangled with python
---
+++ snes.mjs
@@ -0,0 +1,113 @@
+import * as retro from "https://cdn.skypack.dev/pin/snes9x-next@v1.0.0-cli3XObByFqiqSouAHTv/mode=imports,min/optimized/snes9x-next.js";
+import * as thingpixel from "https://cdn.skypack.dev/pin/@thi.ng/pixel@v4.2.7-YzsdE4qjK7uUqur4AuyF/mode=imports,min/optimized/@thi.ng/pixel.js";
+
+export function emulateSnesConsole(romBytes, container) {
+  const emulator = new EventTarget();
+  emulator.retro = retro;
+  const input_state = (emulator.input_state = {});
+
+  const av_info = retro.get_system_av_info();
+
+  const canvas = (emulator.canvas = document.createElement("canvas"));
+  const width = av_info.geometry.base_width;
+  const height = av_info.geometry.base_height;
+  canvas.setAttribute("width", width);
+  canvas.setAttribute("height", height);
+  canvas.setAttribute("tabindex", 0);
+  container.append(canvas);
+
+  const context = canvas.getContext("2d");
+  const imageData = context.createImageData(width, height);
+
+  const environment_command_names = {};
+  for (let [k, v] of Object.entries(retro)) {
+    if (k.startsWith("ENVIRONMENT")) {
+      environment_command_names[v] = k;
+    }
+  }
+
+  retro.set_environment((cmd, data) => {
+    //console.log('environment', environment_command_names[cmd], data);
+    if (cmd == retro.ENVIRONMENT_GET_LOG_INTERFACE) {
+      return function (level, msg) {
+        console.log("retro log", level, msg);
+      };
+    } else {
+      return true;
+    }
+  });
+
+  retro.set_input_poll(() => {
+    //console.log('input_poll');
+  });
+
+  retro.set_input_state((port, device, input, id) => {
+    //console.log('input_state', port, device, input, id);
+    const key = [port, device, input, id].toString();
+    if (input_state[key]) {
+      return input_state[key];
+    } else {
+      return 0; // not pressed by default
+    }
+  });
+
+  retro.set_video_refresh((data, width, height, pitch) => {
+    //console.log('video_refresh', data, width, height, pitch);
+    const buffer = new thingpixel.IntBuffer(
+      pitch / 2,
+      height,
+      thingpixel.RGB565,
+      data
+    );
+    buffer.getRegion(0, 0, width, height).toImageData(imageData);
+    context.putImageData(imageData, 0, 0);
+  });
+
+  retro.set_audio_sample_batch((left, right, frames) => {
+    //console.log('audio_sample_batch', left, right, frames);
+    return frames;
+  });
+
+  retro.init();
+
+  let running = true;
+  let agent = null;
+  let game = romBytes;
+  let initialState;
+
+  retro.load_game(game);
+
+  /*
+        retro.unserialize(initialState);
+        */
+
+  /*
+        let state = retro.serialize();
+        model.set("result", Array.from(state));
+        model.save_changes();
+        */
+
+  function tick() {
+    if (running) {
+      try {
+        emulator.dispatchEvent(new Event("beforeRun"));
+        retro.run();
+        emulator.dispatchEvent(new Event("afterRun"));
+      } catch (err) {
+        console.log("err", err);
+      }
+      requestAnimationFrame(tick);
+    }
+  }
+
+  requestAnimationFrame(tick);
+
+  canvas.addEventListener("click", () => {
+    running = !running;
+    if (running) {
+      requestAnimationFrame(tick);
+    }
+  });
+
+  return emulator;
+}
