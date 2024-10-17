ce51c3c567d22b02adef75d7da2aee34d461a666
Glitch (snes-agent-ui) <none>
2024-10-17 20:19:56 +0000

    🕶🙍 Checkpoint ./smmaps.html:61099663/1333:70889106/10 ./index.html:70889106/676 ./map_tools.mjs:61099663/274

---
+++ map_tools.mjs
@@ -0,0 +1,16 @@
+map_area_offsets = {
+0: (3, 10),
+1: Coord(0, 28),
+2: Coord(31, 48),
+3: Coord(37, 0),
+4: Coord(28, 28),
+5: Coord(0, 10),
+//Out of bounds
+6: Coord(0, -10),
+7: Coord(0, 0),
+};
+
+export function abstractify_pos_global(emulator, map_area_offsets) {
+  
+  
+}
