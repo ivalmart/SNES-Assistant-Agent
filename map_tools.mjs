d699a7e74f43ee2875c655382a5d3c474e6a56da
Glitch (snes-agent-ui) <none>
2024-10-17 20:54:09 +0000

    🍫🦄 Checkpoint ./map_tools.mjs:61099663/233 ./smmaps.html:61099663/1

--- map_tools.mjs
+++ map_tools.mjs
@@ -1,37 +1,40 @@
 export var map_area_offsets = {
-0: (3, 10),
-1: (0, 28),
-2: (31, 48),
-3: (37, 0),
-4: (28, 28),
-5: (0, 10),
+0: [3, 10],
+1: [0, 28],
+2: [31, 48],
+3: [37, 0],
+4: [28, 28],
+5: [0, 10],
 //Out of bounds
-6: (0, -10),
-7: (0, 0),
+6: [0, -10],
+7: [0, 0],
 };
 
 function abstractify_pos(dv) {  
-  var x_radius = dv.getUint16(0x0afe);
-  var y_radius = dv.getUint16(0x0b00);
-  var x_center = dv.getUint16(0x0af6);
-  var y_center = dv.getUint16(0x0afa);
+  var x_radius = dv.getInt16(0x0afe);
+  var y_radius = dv.getInt16(0x0b00);
+  var x_center = dv.getInt16(0x0af6);
+  var y_center = dv.getInt16(0x0afa);
+  console.log([x_radius, y_radius, x_center, y_center]);
   var top = (y_center - y_radius) / 16;
   var left = (x_center - y_radius) / 16;
-  return (top, left);
+  return [top, left];
 }
 
 export function abstractify_pos_global(dv, map_area_offsets) {
   // Area pos
   var area_index = dv.getUint8(0x079F);
   var aoffset = map_area_offsets[area_index];
-  var area_pos = (16 * aoffset[0], 16 * aoffset[1]);
+  var area_pos = [16 * aoffset[0], 16 * aoffset[1]];
   // Map pos
   var map_x = dv.getUint8(0x07A1);
   var map_y = dv.getUint8(0x07A3);
-  var map_pos = (16 * map_x, 16 * map_y);
+  var map_pos = [16 * map_x, 16 * map_y];
   // Room pos
+  console.log("start");
   var room_pos = abstractify_pos(dv);
-  var pos = [area_pos[0] + map_pos[0] + room_pos[0], area_pos[1] + map_pos[1] + room_pos[1])]
+  console.log(room_pos);
+  var pos = [area_pos[0] + map_pos[0] + room_pos[0], area_pos[1] + map_pos[1] + room_pos[1]]
   console.log(pos);
   return pos;
 }
