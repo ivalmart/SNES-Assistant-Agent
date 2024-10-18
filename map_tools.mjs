36b5c1a4f3212912379fb4f8879ee5ca85ef4215
Glitch (snes-agent-ui) <none>
2024-10-18 19:36:26 +0000

    👦🐏 Checkpoint ./.glitch-assets:70889106/1470 ./index.html:4761417/243:70889106/186 ./smmaps.html:70889106/3 ./map_tools.mjs:4761417/126

--- map_tools.mjs
+++ map_tools.mjs
@@ -1,4 +1,4 @@
-export var map_area_offsets = {
+export const map_area_offsets = {
 0: [3, 10],
 1: [0, 28],
 2: [31, 48],
@@ -11,30 +11,30 @@ export var map_area_offsets = {
 };
 
 function abstractify_pos(dv) {  
-  var x_radius = dv.getInt16(0x0afe, true);
-  var y_radius = dv.getInt16(0x0b00, true);
-  var x_center = dv.getInt16(0x0af6, true);
-  var y_center = dv.getInt16(0x0afa, true);
+  const x_radius = dv.getInt16(0x0afe, true);
+  const y_radius = dv.getInt16(0x0b00, true);
+  const x_center = dv.getInt16(0x0af6, true);
+  const y_center = dv.getInt16(0x0afa, true);
   //console.log([x_radius, y_radius, x_center, y_center]);
-  var top = (y_center - y_radius) / 16;
-  var left = (x_center - y_radius) / 16;
+  const top = (y_center - y_radius) / 16;
+  const left = (x_center - y_radius) / 16;
   return [left, top];
 }
 
 export function abstractify_pos_global(dv, map_area_offsets) {
   // Area pos
-  var area_index = dv.getUint8(0x079F);
-  var aoffset = map_area_offsets[area_index];
-  var area_pos = [16 * aoffset[0], 16 * aoffset[1]];
+  const area_index = dv.getUint8(0x079F);
+  const aoffset = map_area_offsets[area_index];
+  const area_pos = [16 * aoffset[0], 16 * aoffset[1]];
   // Map pos
-  var map_x = dv.getUint8(0x07A1);
-  var map_y = dv.getUint8(0x07A3);
-  var map_pos = [16 * map_x, 16 * map_y];
+  const map_x = dv.getUint8(0x07A1);
+  const map_y = dv.getUint8(0x07A3);
+  const map_pos = [16 * map_x, 16 * map_y];
   // Room pos
   console.log("start");
-  var room_pos = abstractify_pos(dv);
+  const room_pos = abstractify_pos(dv);
   //console.log(room_pos);
-  var pos = [area_pos[0] + map_pos[0] + room_pos[0], area_pos[1] + map_pos[1] + room_pos[1]]
+  const pos = [area_pos[0] + map_pos[0] + room_pos[0], area_pos[1] + map_pos[1] + room_pos[1]]
   //console.log(pos);
   return pos;
 }
